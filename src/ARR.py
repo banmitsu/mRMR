31be0c77726f32fa380f4036aefbb534b5642961
banmitsu <banmitsu@gmail.com>
2015-11-25 15:06:06 +0800

    v1

---
+++ src/ARR.py
@@ -0,0 +1,62 @@
+import sys, os
+import moduleinfo
+
+X = []
+Y = []
+##
+# Parse Arrhythmia Data Set
+# Number of Instances: 452 (len(X)); (len(Y))
+# Number of Attributes:279 (len(X[i])
+# Number of Class: 01-16 (or 01 , [02-15], 16(ignore))
+with open('../data/arrhythmia/arrhythmia.data') as f:
+	profile = f.readlines()
+for instance in profile:
+	data = instance.rstrip().split(',')
+	if ( data[-1] != '16' ):
+		X.append(data[:279])
+		if ( data[-1] == '1' ):
+			Y.append('1')
+		else:
+			Y.append('0')
+
+try:
+	f = open('../data/arrhythmia/arrhythmia.bin', 'w')
+except:
+	print "open file error...", '../data/arrhythmia/arrhythmia.bin'
+# Compute the number of bins (histogram) of each variable:
+# save to the file: "arrhythmia.bin"
+X_n = []
+for i in range(len(X[0])):
+	X_n.append([])
+	for n, test in enumerate(X):
+		X_n[i].append(X[n][i])
+	#classet = sorted(set(X_n[i]))
+	#f.write(str(len(classet))+':')
+	#for c in classet:
+	#	f.write(str(X_n[i].count(c))+' ')
+	#f.write('\n')
+
+##
+# Compute the mutual information of each feature space
+# Number of MutualInformation: 279 (len(I_i))
+# Compute the mutual information of pair-feature space
+# Self-information = entropy
+#info = moduleinfo.mutualInformation(X_n[0], X_n[0]) #=5.9032
+#info = moduleinfo.mutualInformation(X_n[0], Y) #=0.2023
+
+for i in range(len(X_n)):
+	f.write(str(i)+':')
+	# mutual information of Xi, Y
+	info = moduleinfo.mutualInformation(X_n[i], Y)
+	I_i.append( info )
+	f.write( str(info)+' ' )
+	# mutual information of Xi, Xj
+	pair = moduleinfo.Pair(i, X_n)
+	cross_info = pair.mutualInformation()
+	f.write( ' '.join(str(info) for info in cross_info))
+	f.write( '\n')
+	#break
+
+f.close()
+# save to data/arrhythmia/arrhythmia.bin
+
---
+++ src/moduleinfo.py
@@ -0,0 +1,79 @@
+import numpy as np
+import math
+
+def mutualInformation(X, Y):
+	info = 0
+	for c1 in set(X):
+		for c2 in set (Y):
+			x_c1 = [x==c1 for x in X]
+			y_c2 = [y==c2 for y in Y]
+			Px = np.mean( x_c1 )
+			Py = np.mean( y_c2 )
+			Pxy= np.mean( np.logical_and(x_c1, y_c2) )
+			if Pxy != 0 and Px !=0 and Py !=0:
+				info += Pxy * math.log( Pxy/ (Px*Py) )
+			#print 'C1: ', c1, '+C2:', c2
+			#print Px, Py, Pxy
+	return info/math.log(2) # according to mRMR(?)
+	#d_x = density(X)
+	#d_y = density(Y)
+	#print d_y, len(d_y)
+
+def density(X):
+	d = []
+	num_data = len(X)
+	classet = sorted(set(X))
+	for c in classet:
+		d.append(float(X.count(c))/num_data)
+	return d
+
+def data_histogram(X):
+	print "data histogram..."
+	classet = sorted(set(X))
+	for c in classet:
+		print 'The class #', c , 'count:', X.count(c)
+
+class Pair:
+	def __init__(self, i, Xn):
+		self.X  = Xn[i]
+		self.Xn = Xn
+		self.Mxy = []
+		self.crossInfo = []
+
+	def mutualInformation(self):
+		# for each Xi
+		for i in range(len(self.Xn)):
+			print 'Processing X', i
+			info = 0
+			for c1 in set(self.X):
+				for c2 in set(self.Xn[i]):
+					x_eq_c1 = [x==c1 for x in self.X]
+					y_eq_c2 = [y==c2 for y in self.Xn[i]]
+					self.Mxy.append( np.mean( np.logical_and(x_eq_c1, y_eq_c2) ) )
+					Px = np.mean( x_eq_c1 )
+					Py = np.mean( y_eq_c2 )
+					Pxy= np.mean( np.logical_and( x_eq_c1, y_eq_c2 ) )
+					if Pxy != 0 and Px!=0 and Py!=0:
+						info += Pxy * math.log( Pxy/(Px*Py) )
+			self.crossInfo.append(info/math.log(2))
+		return self.crossInfo
+
+#def density(x, X):
+#	h = 0.25
+#	N = X.shape[0]
+#	accumulation = 0
+
+#	for x_i in X:
+#		accumulation += math.exp( -(x-x_i)**2/ (2*(h**2)) )/(h*math.sqrt(2*math.pi))
+#	return accumulation/float(N)
+
+
+#if __name__ == '__main__':
+#	discrete_density(X)
+#	# Given 100 sample of a variable x.
+#	X = np.random.rand(3)
+#	print X
+#	# Def. of the density function p(x).
+#	print density(0.5, X)
+#	# Def. of the mutual information.
+#	#print mutualInformation(x, y)
